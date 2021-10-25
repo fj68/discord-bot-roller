24b9fc4dd71cc895d28bfd5b35142ac9ebb74490
fj68 <fj68forg+gitlab@gmail.com>
2021-10-25 10:29:13 +0900

    back to Bot from Client

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from discord import Client
+from discord.ext.commands import Bot
 from discord_slash import SlashCommand
 import os
 import traceback
@@ -29,7 +29,7 @@ def main(token=None, name=None, prefix=None):
     if TOKEN is None:
         raise RuntimeError('Token for Discord Bot API is not provided.')
     
-    bot = Client()
+    bot = Bot()
     slash = SlashCommand(bot)
     
     @bot.event
